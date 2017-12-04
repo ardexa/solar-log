e79aee3f6a7ba607993cabd789c1b4bac62841ad
George Cora <george.cora@ardexa.com>
2017-12-04 17:02:54 +1100

    Readme changes. And added support for older Soalr Log models

--- readme.md
+++ readme.md
@@ -1,47 +1,48 @@
 
 # Purpose
-The purpose of this project is to collect from Solar Log devices and send the data to your cloud using Ardexa. Data from Solar Log is read using a Linux device such as a Raspberry Pi, or an X86 intel powered computer. 
+The purpose of this project is to collect data from Solar Log devices and send the data to the cloud using Ardexa. Data from Solar Log is read using a Linux device such as a Raspberry Pi, or an X86 intel powered computer. 
 
 ## How does it work
-A Solar Log device allows data to be collected from solar inverters. See -> http://www.solar-log.com/en/home.html. This python script allows data to be collected and either stored locally and/or sent to another site, and/or sent to the Ardexa cloud.
-The python script has been tested on Linux systems. The script has been tested on a Solar-1200. If there are any issues with other Solar Log devices, please let us know.
+A Solar Log device is a product that allows data to be collected from solar inverters. See -> http://www.solar-log.com. This product does not make it easy to store data locally. This project allows data to be collected and either stored locally and/or sent to the Ardexa cloud. These python scripts have been tested on Linux systems and a number of Solar Log devices. If there are any issues with other Solar Log devices, please let us know.
 
 This script will take an IP address of the Solar Log device, and do the following:
-- Query a Solar Log device and call down all available data. The Solar Log stores a maximum of 1 month's worth of data.
+- Query a Solar Log device and call down all available data using http.
 - Compare the changes to the previous run (so the first run will NOT produce any data)
-- Send any changes to a local file and to the cloud via the Ardexa agent
-- All data is stored in the /opt/ardexa/solar-log directory. Data is stored in a daily file
-- A check to make sure it can only run once
-- Extract data from connected SMA, ABB or Refusol inverters. Other inverters should be supported. If they are not, please contact us
+- Send any changes to a local file and/or to the cloud via the Ardexa agent
+- All data is stored in a user specified log directory.
+- A check to make sure it can only run once. 
+- Extract data from connected SMA, ABB or Refusol inverters. Other inverters can be supported. If they are not, please contact us
 
-The Solar Log will take about 5 minutes to download a month's worth of data (yes, about 5 minutes!). So the frequency (time between runs) of the script should not be lower than about 6 minutes. Also, 
-data from the inverter is copied by the Solar Log every 5 minutes. This is not configurable in Solar Log.
+The Solar Log will take about 5 to 10 minutes to download a month's worth of data (yes, that long!). So the frequency (time between runs) of the script should not be lower than about 10 minutes. Some of the newer models will return data in less than 5 minutes. Also, data from the inverter is copied by the Solar Log every 5 minutes. This is not configurable in Solar Log.
 
-Make sure the Solar Log is installed and running, as per the manufacturer's instructions.
-
-## Configure the script
-Firstly, make sure you have the dependancies to run the script
+## Installation
+To install the scripts, do the following
 ```
 sudo apt-get update
 sudo apt-get install -y python-pip
 sudo pip install requests
-```
-
-Then install and run it as follows:
-Note that the applications should be run as root only since needs access to a device in the `/dev` directory. 
-```
 cd
 git clone https://github.com/ardexa/solar-log.git
-cd solar-log
-sudo python ./ardexa-solar-log.py {inverter-type} {IP Address} .... example: `sudo python ./ardexa-solar-log.py refusol 192.168.0.109`
+cd solar-log/script
 ```
 
-The `{inverter-type}` is either `SMA`, `ABB` or `Refusol` inverters. The `{IP Address}` is the IP address of the Solar Log device. Data will be written to the directory `/opt/ardexa/solar-log/logs/`, based on inverter number.
+## Running the script
+The script does not need root privileges. So you an run it as follows:
+```
+python ardexa-solar-log.py {IP address} {inverter type} {solar-log-type} {log directory} {debug} 
+example: `python ardexa-solar-log.py 192.168.1.55 ABB new /opt/ardexa 0`
+```
+
+- The `IP Address` is the IP address of the Solar Log device. 
+- The `inverter-type` is either `SMA`, `ABB` or `Refusol` inverters. If you need other inverter types, contact us.
+- The 'solar log type is either `NEW` or `OLD`. Older Solar Log devices with a software firmware of less than about 3.0 (Build 60. March 2014), then that is an `OLD` version. Also note that the `OLD` style Solar Log devices may stop recording when the inverter is turned off (ie; when the Sun goes down), whereas the `NEW` inverters will write a record all times.
+- The `log directory` is where data will be written.
+- Debug is a number between 0 (no debug) and 3
 
 ## Collecting to the Ardexa cloud
 Collecting to the Ardexa cloud is free for up to 3 Raspberry Pis (or equivalent). Ardexa provides free agents for ARM, Intel x86 and MIPS based processors. To collect the data to the Ardexa cloud do the following:
-a. Create a `RUN` scenario to schedule the Ardexa Kaco script to run at regular intervals, but no less than about 330 seconds.
-b. Then use a `CAPTURE` scenario to collect the csv (comma separated) data from the filename `/opt/ardexa/solar-log/logs/{inverter}/latest.csv`. This file contains a header entry (as the first line) that describes the CSV elements of the file.
+- Create a `RUN` scenario to schedule the Ardexa Kaco script to run at regular intervals, but no less than about 330 seconds.
+- Then use a `CAPTURE` scenario to collect the csv (comma separated) data from the filename `/opt/ardexa/solar-log/logs/{inverter}/latest.csv`. This file contains a header entry (as the first line) that describes the CSV elements of the file.
 
 ## Help
 Contact Ardexa at support@ardexa.com, and we'll do our best efforts to help.
--- script/ardexa-solar-log.py
+++ script/ardexa-solar-log.py
@@ -12,10 +12,9 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 #
-
-# This script will query a 'solar-log' device. Usage: sudo python ./ardexa-solar-log.py {inverter-type} {IP Address}
-# eg: sudo python ./ardexa-solar-log.py refusol 192.168.0.109
-# eg; sudo python ./ardexa-solar-log.py SMA 192.168.0.10
+#
+# This script will query a 'solar-log' device. Usage: python ardexa-solar-log.py {IP address} {inverter type} {solar-log-type} {log directory} {debug}
+# eg: python ardexa-solar-log.py 192.168.1.55 ABB new /opt/ardexa 0
 #
 # For use on Linux systems
 # Make sure the following tools have been installed
@@ -30,13 +29,12 @@ from shutil import copyfile
 import os
 import urllib2
 import datetime
-
-# Change these 2 settings to suit your installation, if required
-DEBUG = 0
-LOG_DIR = '/opt/ardexa/solar-log/logs/'
+from Supporting import *
 
 
-DIR = '/opt/ardexa/solar-log/'
+PIDFILE = 'ardexa-solar-log-'
+# Usage example: eg; python ardexa-solar-log.py 192.168.1.55 ABB new /opt/ardexa 0
+USAGE = "python ardexa-solar-log.py {IP address} {inverter type} {solar-log-type} {log directory} {debug}"
 LAST = 'last.csv'
 CURRENT = 'current.csv'
 COMPLETE_STRING = "\"777\":3,"
@@ -47,7 +45,6 @@ REFUSOL = 'refusol'
 SMA = 'sma'
 ABB = 'abb'
 
-
 # These next items detail the number of items in a **RAW** event line
 SMA_HEADER_ITEMS = 12
 ABB_HEADER_ITEMS = 13
@@ -56,58 +53,12 @@ REFUSOL_HEADER_ITEMS = 11
 #~~~~~~~~~~~~~~~~~~~   START Functions ~~~~~~~~~~~~~~~~~~~~~~~
 
 
-# This function logs the inverter data to both a 'date' file, and a 'latest' file
-# Directory structure is as follows: LOG_DIR + inverter_type + inverter_number
-def write_log(inverter_type, inverter_number, header, logline):
-	write_header = False
-	# Check date file exists
-	date_str = (time.strftime("%d-%b-%Y"))
-
-	# The logging directory is the LOG_DIR + inverter_type and then inverter_number
-	log_directory = os.path.join(LOG_DIR, inverter_type)
-	log_directory = os.path.join(log_directory, inverter_number)
-	if not os.path.exists(log_directory):
-		os.makedirs(log_directory)
-
-	full_path_date = os.path.join(log_directory, date_str + ".csv")
-	full_path_latest = os.path.join(log_directory, "latest.csv")
-
-	if (DEBUG > 1):
-		print "FULL: ", full_path_date
-		print "LATEST: ", full_path_latest
-
-	# If the date file doesn't exist, create it, and remove the "latest" file
-	if not (os.path.isfile(full_path_date)):
-		if (DEBUG > 1):
-			print "Date file doesn't exist: ", full_path_date, " . Removing the default file"
-			# Remove the default file (beware, it may not exist)
-		if (os.path.isfile(full_path_latest)):
-			os.remove(full_path_latest)
-		write_header = True
-
-
-	# Now create both (or open both) and write to them
-	if (DEBUG >= 1):
-		print "Writing the line: ", logline
-
-	write_latest = open(full_path_latest,"a")
-	write_date = open(full_path_date,"a")
-	if (write_header):
-		write_date.write(header)
-		write_latest.write(header)
-
-	write_date.write(logline)
-	write_latest.write(logline)
-	write_date.close()
-	write_latest.close()
-
-
 # This will fix up the solar-log time. There are problems with it
-def fix_time(time):
+def fix_time(time, debug):
 	hour, minute, rest = time.split(':')
 	seconds, pm = rest.split()
 
-	if (DEBUG > 1):
+	if (debug > 1):
 		print "Before processing.....Hour: ", hour, " Minute: ", minute, " Seconds: ", seconds, " pm: ",pm
 
 	# if pm = PM, convert hour to int, add 12, but NOT if its 12 already
@@ -122,7 +73,7 @@ def fix_time(time):
 	if (len(str(hour)) == 1):
 		hour = '0' + hour
 
-	if (DEBUG > 1):
+	if (debug > 1):
 		print "After processing.....Hour: ", hour, " Minute: ", minute, " Seconds: ", seconds, " pm: ",pm
 
 	return(str(hour) + ":" + str(minute) + ":" + str(seconds))
@@ -132,47 +83,48 @@ def fix_time(time):
 # This function will strip an Inverter line reported by solar-log
 # The line will be in this format: #Date;Time;INV;Pac;DaySum;Status;Error;Pdc1;Udc1;Uac;Idc1;Iac
 # Eg; 27/01/17;2:30:00 PM;1;941;4546;7;0;37301;228;252;4457;3720
-def SMA_line(line):
+def SMA_line(line, debug):
 	
-	if (DEBUG >= 2):
+	if (debug >= 2):
 		print "Raw SMA line: ",line
 
 	date,time,inverter,power,daily,status,error,pdc1,udc1,uac,idc1,iac = line.split(';')
+
 	# Date and time will be in the format: 27/01/17;1:40:00 PM or 27.01.17;1:40:00 PM
 	# Convert the date and time to format: 2017-01-29T13:00:00
 	# There are problems with the SMA/solar-log time implementation:
-	time = fix_time(time)
-	
+	time = fix_time(time, debug)	
 	new_datetime = datetime.datetime.strptime(date + " " + time, '%d/%m/%y  %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%S')
 
 	return_line = str(new_datetime) + "," + str(inverter) + "," + str(power) + "," + str(daily) + "," + str(status) + "," + \
 					  str(error) + "," + str(pdc1) + "," + str(udc1) + "," + str(uac) + "," + str(idc1) + "," + str(iac) + "\n"
 	
-	if (DEBUG >= 2):
+	if (debug >= 2):
 		print "Writing the SMA line: ",return_line
 
 	return return_line
+
 
 # This function is for Refusol inverters
 # This function will strip an Inverter line reported by solar-log
 # The line will be in this format: #Date;Time;INV;Pac;DaySum;Status;Error;Pdc1;Udc1;Temp;Uac;
 # Eg; 01.04.17;12:00:00;1;12703;24000;4;0;12935;565;52;235;
-def REFUSOL_line(line):
+def REFUSOL_line(line, debug):
 	
-	if (DEBUG >= 2):
+	if (debug >= 2):
 		print "Raw Refusol line: ",line
 
 	date,time,inverter,power,daily,status,error,pdc1,udc1,temperature,uac = line.split(';')
+
 	# Date and time will be in the format: 01.04.17;12:05:00 or 01.04.17;04:25:00
 	# Convert the date and time to format: 2017-01-29T13:00:00
 	# Refusol has no problems with date and time
-	
 	new_datetime = datetime.datetime.strptime(date + " " + time, '%d.%m.%y  %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%S')
 
 	return_line = str(new_datetime) + "," + str(inverter) + "," + str(power) + "," + str(daily) + "," + str(status) + "," + \
 					  str(error) + "," + str(pdc1) + "," + str(udc1) + "," + str(temperature) + "," + str(uac) + "\n"
 	
-	if (DEBUG >= 2):
+	if (debug >= 2):
 		print "Writing the Resfusol line: ",return_line
 
 	return return_line
@@ -182,98 +134,31 @@ def REFUSOL_line(line):
 # This function will strip an Inverter line reported by solar-log
 # The line will be in this format: #Date;Time;INV;Pac;DaySum;Status;Error;Pdc1;Pdc2;Udc1;Udc2;Temp
 # Eg; 04.04.17;13:25:00;1;20119;43722;6;0;11275;9359;497;414;48
-def ABB_line(line):
+def ABB_line(line, debug):
 	
-	if (DEBUG >= 2):
+	if (debug >= 2):
 		print "Raw ABB line: ",line
 
 	date,time,inverter,power,daily,status,error,pdc1,pdc2,udc1,udc2,temperature,uac = line.split(';')
+
 	# Date and time will be in the format: 04.04.17;12:05:00 or 01.04.17;04:25:00
 	# Convert the date and time to format: 2017-01-29T13:00:00
 	# ABB has no problems with date and time
-	
 	new_datetime = datetime.datetime.strptime(date + " " + time, '%d.%m.%y  %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%S')
 
 	return_line = str(new_datetime) + "," + str(inverter) + "," + str(power) + "," + str(daily) + "," + str(status) + "," + \
 					  str(error) + "," + str(pdc1) + "," + str(pdc2) + "," + str(udc1) + "," + str(udc2) + "," + str(temperature) + "," + \
 					  str(uac) + "\n"
 
-	if (DEBUG >= 2):
+	if (debug >= 2):
 		print "Writing the ABB line: ",return_line
 	
 	return return_line
 
 
-# Check that the correct number of arguments, and extract the IP Address of solar-log
-def check_args():
-	# Get the IP Address of Solar-Log from the argument
-	if (len(sys.argv) != 3):
-		print "There can only be 3 arguments, the script name, inverter type and the IP Address"
-		return ""
-	else:
-		# return the inverter type and the IP address
-		return str(sys.argv[1]), str(sys.argv[2])
-
-
-# Check that a process is not running more than once, using PIDFILE
-def check_PIDFILE(pidfile):
-
-	# Check PID exists and see if the PID is running
-	if os.path.isfile(pidfile):
-		pidfile_handle = open(pidfile, 'r')
-		try: 
-			pid = int(pidfile_handle.read())
-			if (check_pid(pid)):
-				return True
-		except:
-			# do nothing here
-			pass
-			
-		pidfile_handle.close()
-		# PID is not active, remove the PID file
-		os.unlink(pidfile)
-
-	# Create a PID file, to ensure this is script is only run once (at a time)
-	pid = str(os.getpid())
-	file(pidfile, 'w').write(pid)
-
-	return False
-
-
-# This function will query the solar-log CSV file
-def query_csv(current, ip_addr):
-
-	success = False
-
-	url = 'http://' + ip_addr + '/export_min.csv'
-	if (DEBUG >= 2):		
-		print "URL being used for CSV download: ",url
-
-	try:
-		filew = open(current, "w")
-
-		# Timeout the request 
-		response = requests.post(url, timeout=30)
-		if (response.status_code == 200):
-			if (DEBUG >= 2):
-				print "Got a successful response from the solar-log device"
-			success = True
-			for x in response:
-				filew.write(x)
-			filew.close()
-		else:
-			# The script does sometimes return with a 404 error, due to unknown solar-log issues
-			if (DEBUG >= 2):
-				print "404 error for downloading the CSV file\n"
-	except:
-			# The script does sometimes return with general error, due to unknown solar-log issues
-			if (DEBUG >= 2):
-				print "Error in main URL CSV download request loop\n"	 
-
-	return success
 
 # This function will split the inverters, so that date and time is in each line, but everything is in a separate line
-def split_inverters(line, inverter_type):
+def split_inverters(line, inverter_type, debug, log_directory):
 	items = line.split(';')
 	date = items[0]
 	time = items[1]
@@ -293,19 +178,23 @@ def split_inverters(line, inverter_type):
 	remainder = length % item_number
 	whole = length / item_number
 
-	if (DEBUG >= 2):
+	if (debug >= 2):
 		print "Length: ",length
 		print "Whole: ",whole
 		print "Items: ",items
 		print "Inverter type: ",inverter_type
 
+	# Add inverter type to the log directory
+	log_directory = os.path.join(log_directory, inverter_type)
+
 	# Once the date and time have been removed, the number of items in the line should be divisible by the item_number
-	# If so, process each inverter line separately
-	# Else, report an error
+	# If so, process each inverter line separately. Else, report an error
 	if (remainder == 0):
 		for run in range(whole):
+			converted_line = ""
+			header = ""
 			inverter_slice = items[:item_number]
-			if (DEBUG >= 2):
+			if (debug >= 2):
 				print "Inverter slice: ",inverter_slice
 			del items[:item_number]
 			inverter_slice.insert(0, time)
@@ -313,42 +202,77 @@ def split_inverters(line, inverter_type):
 			inverter_line = ";".join(inverter_slice)
 			inverter_number = inverter_slice[2]
 			if (inverter_type == REFUSOL):
-				converted_line = REFUSOL_line(inverter_line)
-				if (DEBUG >= 2):
-					print "Split line: ",converted_line
-				write_log(inverter_type, inverter_number, REFUSOL_LOG_HEADER, converted_line)
+				converted_line = REFUSOL_line(inverter_line, debug)
+				header = REFUSOL_LOG_HEADER
 
 			elif (inverter_type == SMA):
-				converted_line = SMA_line(inverter_line)
-				if (DEBUG >= 2):
-					print "Split line: ",converted_line
-				write_log(inverter_type, inverter_number, SMA_LOG_HEADER, converted_line)
+				converted_line = SMA_line(inverter_line, debug)
+				header = SMA_LOG_HEADER
 
 			elif (inverter_type == ABB):
-				converted_line = ABB_line(inverter_line)
-				if (DEBUG >= 2):
-					print "Split line: ",converted_line
-				write_log(inverter_type, inverter_number, ABB_LOG_HEADER, converted_line)
+				converted_line = ABB_line(inverter_line, debug)
+				header = ABB_LOG_HEADER
 
+			if (debug >= 2):
+				print "Split line: ",converted_line
+
+			# Add inverter number to the log directory
+			log_directory = os.path.join(log_directory, inverter_number)
+			date_str = time.strftime("%d-%b-%Y")
+			log_filename = date_str + ".csv"
+			write_log(log_directory, log_filename, header, converted_line, debug, True, log_directory, "latest.csv")
 
 	else:
 		print "The line reported by solar-log is not recognised. Here is the line: ", line
-		sys.exit(3)
+
+	return
+
+
+# This function will query the latest solar-log CSV file
+def query_csv(current, ip_addr, debug):
+	success = False
+
+	url = 'http://' + ip_addr + '/export_min.csv'
+	if (debug >= 2):		
+		print "URL being used for CSV download: ",url
+
+	try:
+		filew = open(current, "w")
+
+		# Timeout the request 
+		response = requests.post(url, timeout=30)
+		if (response.status_code == 200):
+			if (debug >= 2):
+				print "Got a successful response from the solar-log device"
+			success = True
+			for x in response:
+				filew.write(x)
+			filew.close()
+		else:
+			# The script does sometimes return with a 404 error, due to unknown solar-log issues
+			if (debug >= 2):
+				print "404 error for downloading the CSV file\n"
+	except:
+			# The script does sometimes return with general error, due to unknown solar-log issues
+			if (debug >= 2):
+				print "Error in main URL CSV download request loop\n"	 
+
+	return success
+
+
 
 # This function will extract any new lines. It operates on the differences between 2 files
 # 'last' and 'current'. If there are any differences (ie; new lines), then they will be processed
 # by this function
-def extract_latest_lines(last, current, inverter_type):
+def extract_latest_lines(last, current, inverter_type, debug, log_directory):
 
-	converted_line = ""
-
-	# If 'last' doesn't exist, then copy 'current' to 'last', and process. No results will be extracted anyway
+	# If 'last' doesn't exist, then copy 'current' to 'last', and return
 	if not (os.path.isfile(last)):
 		# Copy 'current.csv' to 'last.csv'
-			copyfile(current, last)
-			if (DEBUG >= 1):
-				print '\'last.csv\' file doesn\'t exist. Copy across the \'current.csv\'. file.'
-
+		copyfile(current, last)
+		if (debug >= 1):
+			print '\'last.csv\' file doesn\'t exist. Copying across the \'current.csv\'. file.'
+		return
 
 	# If both files exist, process them. Detect any differences.
 	if ((os.path.isfile(last)) and (os.path.isfile(current))):
@@ -358,28 +282,27 @@ def extract_latest_lines(last, current, inverter_type):
 
 		current_file.close()
 		last_file.close()
-
-		localtime = time.asctime( time.localtime(time.time()) )
+		
 		for line in diff:
 			newline = line.strip()
-			if (DEBUG >= 1):
-				print "New Line at time: ",localtime," Line: ",newline
+			if (debug >= 1):
+				datetime = get_datetime()
+				print "New Line at time: ",datetime," Line: ",newline
 
-			split_inverters(newline, inverter_type)
+			split_inverters(newline, inverter_type, debug, log_directory)
 
 
 		# Copy 'current.csv' to 'last.csv'
 		copyfile(current, last)
 
-		# For this test, append line diff into a file
-
-	return converted_line	
+	return 	
 
 
-def prepare(ip_addr):
+# For the new types of Solar Log, issue a prepare URL and wait for it to finish
+def prepare_new(ip_addr, debug):
 	# URL to use for this query
 	url = 'http://' + ip_addr + '/getjp'
-	if (DEBUG >= 2):		
+	if (debug >= 2):		
 		print "URL being used for PREPARE: ",url
 
 	try:
@@ -403,85 +326,118 @@ def prepare(ip_addr):
 			for line in response:
 				if (line.find(COMPLETE_STRING) != -1):
 					complete = True
-				if (DEBUG > 1):
+				if (debug > 1):
 					print "Result from URL query: ",line," and complete status: ",complete				
 						 	
 			response.close()
 			attempt += 1
 			
-			# If it takes more than about 8 minutes, bail out
-			if (attempt > 50):
+			# If it takes more than about 12 minutes, bail out
+			if (attempt > 72):
 				complete = True
 
 	except:
-		print "404 error for \'prepare\'\n"
+		if (debug >= 1):
+			print "404 error for \'prepare\'\n"
 
-# This function will check whether a PID is currently running
-def check_pid(pid):  	
 
-	try:
-		# A Kill of 0 is to check if the PID is active. It won't kill the process
-		os.kill(pid, 0)
-		if (DEBUG > 1):
-			print "Script has a PIDFILE where the process is still running"
-		return True
-	except OSError:
-		if (DEBUG > 1):
-			print "Script does not appear to be running"
-		return False
-	
+# For the old types of Solar Log, issue a prepare URL and wait about 10 minutes
+def prepare_old(ip_addr, debug):
+	# URL to use for this query
+	url = 'http://' + ip_addr + '/expcsv.dat?1'
+	if (debug >= 2):		
+		print "URL being used for PREPARE: ",url
+
+	# Request data from Solar Log 
+	req = urllib2.Request(url)
+	response = urllib2.urlopen(req)
+
 
 #~~~~~~~~~~~~~~~~~~~   END Functions ~~~~~~~~~~~~~~~~~~~~~~~
 
-# If the operating and logging directories don't exist, create them
-if not os.path.exists(DIR):
-	os.makedirs(DIR)
+# Check the arguments
+arguments = check_args(5)
+if (len(arguments) < 5):
+	print "The arguments cannot be empty. Usage: ", USAGE
+	sys.exit(3)
 
-if not os.path.exists(LOG_DIR):
-	os.makedirs(LOG_DIR)
+IP_address = arguments[1]
+inverter_type = arguments[2]
+solar_log_type = arguments[3] 
+log_directory = arguments[4]
+debug_str = arguments[5]
 
-inverter_type, ip_address = check_args()
-# if IP address is empty, exit with error
-if not ip_address:
-	print "The IP Address cannot be empty"
-	sys.exit(2)
+# Convert debug
+retval, debug = convert_to_int(debug_str)
+if (not retval):
+	print "Debug needs to be an integer number. Value entered: ",debug_str
+	sys.exit(4)
+
+# if any args are empty, exit with error
+if ((not IP_address) or (not inverter_type) or (not log_directory) or (not solar_log_type)):
+	print "The arguments cannot be empty. Usage: ", USAGE
+	sys.exit(5)
+
+# If the logging directory doesn't exist, create it
+if (not os.path.exists(log_directory)):
+	os.makedirs(log_directory)
+
+# Check that no other scripts are running
+pidfile = os.path.join(log_directory, PIDFILE) + IP_address + ".pid"
+if check_pidfile(pidfile, debug):
+	print "This script is already running"
+	sys.exit(6)
+
+solar_log_type = solar_log_type.lower()
+if ((solar_log_type != 'old') and (solar_log_type != 'new') ):
+	print "Solar Log Type can only be \'NEW\' or \'OLD\'"
+	sys.exit(7)
 
 inverter_type = inverter_type.lower()
 if ((inverter_type != SMA) and (inverter_type != REFUSOL) and (inverter_type != ABB) ):
 	print "Only Refusol, ABB or SMA inverters collected by Solar-Log are supported at this time"
-	sys.exit(3)
-
-# Check that no other scripts are running
-pidfile =  str(DIR + 'ardexa-solar-log.pid')
-if check_PIDFILE(pidfile):
-	print "This script is already running"
-	sys.exit(4)
-
+	sys.exit(8)
 
 start_time = time.time()
 # Issue a query to get the solar-log to 'prepare' the CSV file
-prepare(ip_address)
+if (solar_log_type == 'new'):
+	prepare_new(IP_address, debug)
+else:
+	prepare_old(IP_address, debug)
+
+# Define the current and last files
+last_file = os.path.join(log_directory, LAST)
+current_file = os.path.join(log_directory, CURRENT)
 
 # Try to download the CSV file from the solar-log device	
-success = query_csv(DIR + CURRENT, ip_address)
+success = query_csv(current_file, IP_address, debug)
 
-line = ""
 if (success):
 	# If CSV file could be extracted, compare this run to the last
-	line = extract_latest_lines(DIR + LAST, DIR + CURRENT, inverter_type)
+	# New lines will then be written to file
+	line = extract_latest_lines(last_file, current_file, inverter_type, debug, log_directory)
 
 elapsed_time = time.time() - start_time
-if (DEBUG >= 1):
+if (debug > 0):
 	print "This request took: ",elapsed_time, " seconds."
-	print "IP address used: ", ip_address, " and inverter type: ", inverter_type
+	print "IP address used: ", IP_address, " and inverter type: ", inverter_type
 
 # Remove the PID file	
 if os.path.isfile(pidfile):
 	os.unlink(pidfile)
 
-# Print the line if one is available, otherwise the success of the 'query_csv' command
-if not line:
-	print success
-else:
-	print line
+sys.exit(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
